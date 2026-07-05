4c76a238565bc2ce97e24a2ec7b1a7575e557661
bagaswap111 <81802233+bagaswap111@users.noreply.github.com>
2026-07-05 18:58:58 +0700

    fix: Option A - pre-compile Flutter plugin with AGP bundled, apply all plugins via apply plugin:

--- maya_on_the_fly/macos/Flutter/GeneratedPluginRegistrant.swift
+++ maya_on_the_fly/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,16 +7,18 @@ import Foundation
 
 import flutter_secure_storage_macos
 import git2dart_binaries
-import path_provider_foundation
+import google_sign_in_ios
+import local_auth_darwin
 import printing
 import share_plus
-import sqflite
+import sqflite_darwin
 import sqlcipher_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   Git2dartBinariesPlugin.register(with: registry.registrar(forPlugin: "Git2dartBinariesPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
